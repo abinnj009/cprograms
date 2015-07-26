2531e9c3e7687ddf2c2b515b1e6046dfb90d203a
Abin <abinnj009@gmail.com>
2015-07-26 13:56:43 +0530

    kthread_stop(), is indeed a blocking call. It waits for the thread to exit and since our thread is in while(1), so hopefully, it will never exit and unfortunately, our rmmod will never come out. So, what does this mean? What we can infer from this, is that the kthread_stop() is just the signal, not the command. Calling kthread_stop() doesn’t gives you a license to kill/stop the thread, instead it just sets the flag in the task_struct() of the thread and waits for the thread to exit. It’s totally upto the thread to decide, when it would like to exit.  So, why is such a thing? Well, just think of the scenario where kernel thread has allocated a memory and would free it up once it exits. Had it been allowed to be killed in middle, thread would never be able to free up the memory. This, in turn would result in memory leak. This was the one of the simplest scenarios, which I could think of. Coming back to our problem, how do we get back the command prompt? Let’s try one more thing. In the user space, you might have used the kill  command to send the signal to the process. And one of the most powerful signal which process can’t mask is SIGKILL. So, lets use the same on the kernel thread as well. Find the id of the running kernel thread with ps command and then, use the following command:
    kill -9 <thread_id>
    
    So, what’s the result? Dude … this thread is invincible!. True, by default, kernel thread ignores all the signals. The reason behind this is same as explained above. Kernel thread has a full control over when can it be killed. So, the only way to get out of this problem is to kill the problem, that means, reboot the system

---
+++ thread-advanced/thread.c
@@ -0,0 +1,44 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+
+static struct task_struct *thread_st;
+
+//fn executed by the kernel thread
+static int thread_fn(void *unused)
+{
+	while(1)
+	{
+		printk(KERN_INFO "Thread Running\n");
+		ssleep(5);
+	}
+	printk(KERN_INFO "thread stopping\n");
+	do_exit(0);
+	return 0;
+}
+//module initialization
+static int __init init_thread(void)
+{
+	printk(KERN_INFO "Creating thread\n");
+	//create the kernel thread with name 'mythread'
+	thread_st = kthread_run(thread_fn, NULL, "mythread");
+	if(thread_st)
+		printk(KERN_INFO "thread created succesfully");
+	else
+		printk(KERN_ERR "thread creation failed");
+	return 0;
+}
+
+//module exit
+static void __exit cleanup_thread(void)
+{
+	printk(KERN_INFO "cleaning up\n");
+	if(thread_st)
+	{
+		kthread_stop(thread_st);
+		printk(KERN_INFO "THREAD stopped");
+	}
+}
+module_init(init_thread);
+module_exit(cleanup_thread);
